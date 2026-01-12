f441af80385b653c016b47064f9c5ebd2d361286
D-4costa <danielleelisabeth01@gmail.com>
2026-01-12 15:42:09 -0500

    Change output directory to 'dist'

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from "path";
 
 export default {
   build: {
-    outDir: "../dist",
+outDir: "dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
